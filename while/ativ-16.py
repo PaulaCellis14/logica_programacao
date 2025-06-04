7b2c3f6114945e2b8522e36726002028792ded92
Paula Cellis <146037300+PaulaCellis14@users.noreply.github.com>
2025-06-04 13:58:47 -0300

    add a new file

--- while/ativ-16.py
+++ while/ativ-16.py
@@ -1,9 +1,16 @@
 #Faça um programa que use um laço while para exibir os primeiros 20 termos da sequência de Fibonacci.
 
-fibonacci_term = 0
-increment = 1
+temp_term = 0
+previous_term = 0
+current_term = 1
+counter = 1
 
-while increment <= 20:
-    print(fibonacci_term)
+while counter <= 20:
+    print(previous_term)
+    temp_term = previous_term
+    previous_term = current_term
+    current_term = temp_term + previous_term
+    counter += 1
+
     
 
